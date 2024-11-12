442fe9a09d6f2c8444cf865fc7f83698be0b6609
Fraser McLean <dev@frasermclean.com>
2024-11-12 22:44:05 +1100

    Improve posts list empty list handling

--- frontend/src/app/components/post-list/post-list.component.ts
+++ frontend/src/app/components/post-list/post-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { PostsService } from '@services/posts.service';
@@ -8,7 +9,7 @@ import { PostItemComponent } from './post-list-item/post-list-item.component';
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [PostItemComponent, AsyncPipe, MatProgressSpinnerModule],
+  imports: [PostItemComponent, AsyncPipe, MatButtonModule, MatProgressSpinnerModule],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.scss',
 })
@@ -16,4 +17,8 @@ export class PostListComponent {
   private readonly postsService = inject(PostsService);
   posts$ = this.postsService.getPosts();
   loading$ = this.postsService.busy$;
+
+  refreshPosts() {
+    this.posts$ = this.postsService.getPosts();
+  }
 }
