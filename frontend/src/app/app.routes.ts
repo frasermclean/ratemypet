428d2e45dae0c8374cfccb29b173e761c5091fb6
Fraser McLean <dev@frasermclean.com>
2024-11-14 08:46:54 +1100

    Update routing paths for LoginComponent to use '/auth/login'

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { PostListComponent } from './components/post-list/post-list.component';
 import { PostViewComponent } from './components/post-view/post-view.component';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './auth/login/login.component';
 
 export const routes: Routes = [
   { path: 'posts/:postId', component: PostViewComponent },
   { path: 'posts', component: PostListComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'auth/login', component: LoginComponent },
   { path: '', redirectTo: '/posts', pathMatch: 'full' }
 ];
