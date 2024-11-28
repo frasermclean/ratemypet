cba5847558b17398167547bdb6dc6c2e5e891617
Fraser McLean <dev@frasermclean.com>
2024-11-28 18:36:52 +0800

    Refactor PostViewComponent to improve status handling and layout; update styles for better responsiveness and readability

--- frontend/src/app/posts/post-view/post-view.component.ts
+++ frontend/src/app/posts/post-view/post-view.component.ts
@@ -1,28 +1,25 @@
-import { Component, inject, input, OnInit } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
+import { Component, input, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { Store } from '@ngxs/store';
-import { Observable } from 'rxjs';
+import { dispatch, select } from '@ngxs/store';
 
 import { PostsActions } from '../posts.actions';
 import { PostsState } from '../posts.state';
-import { DetailedPost } from '@models/post.models';
 
 @Component({
   selector: 'app-post-view',
   standalone: true,
-  imports: [AsyncPipe, MatCardModule, MatProgressSpinnerModule],
+  imports: [MatCardModule, MatProgressSpinnerModule],
   templateUrl: './post-view.component.html',
   styleUrl: './post-view.component.scss',
 })
 export class PostViewComponent implements OnInit {
-  private readonly store = inject(Store);
-  postId = input.required<string>();
-  status$ = this.store.select(PostsState.status);
-  post$ = this.store.select(PostsState.currentPost);
+  readonly postId = input.required<string>();
+  readonly status = select(PostsState.status);
+  readonly post = select(PostsState.currentPost);
+  private readonly getPost = dispatch(PostsActions.GetPost);
 
   ngOnInit(): void {
-    this.store.dispatch(new PostsActions.GetPost(this.postId()));
+    this.getPost(this.postId());
   }
 }
