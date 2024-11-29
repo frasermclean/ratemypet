7831fb54c870fc0a9b8ee025cc8f4c236c6df3d6
Fraser McLean <dev@frasermclean.com>
2024-11-29 09:36:15 +0800

    Add footer component and restructure app layout; implement grid display for main content

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { ToolbarComponent } from './shared/toolbar/toolbar.component';
+import { FooterComponent } from "./shared/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarComponent],
+  imports: [RouterOutlet, ToolbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
