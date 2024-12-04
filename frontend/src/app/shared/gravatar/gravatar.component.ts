794e9f9f289f5db1a35c337a31cea9b67893474f
Fraser McLean <dev@frasermclean.com>
2024-12-04 12:28:38 +0800

    Refactor Gravatar component: rename emailOrHash input to emailHash and update related usages

--- frontend/src/app/shared/gravatar/gravatar.component.ts
+++ frontend/src/app/shared/gravatar/gravatar.component.ts
@@ -1,5 +1,4 @@
 import { Component, input } from '@angular/core';
-import { sha256 } from 'js-sha256';
 
 @Component({
   selector: 'app-gravatar',
@@ -9,18 +8,13 @@ import { sha256 } from 'js-sha256';
   styleUrl: './gravatar.component.scss',
 })
 export class GravatarComponent {
-  emailOrHash = input.required<string>();
+  emailHash = input.required<string>();
   size = input(40);
   defaultImage = input<'404' | 'mp' | 'identicon' | 'monsterid' | 'wavatar' | 'retro' | 'robohash' | 'blank'>('retro');
   rating = input<'g' | 'pg' | 'r' | 'x'>('g');
   shape = input<'circle' | 'rounded'>('circle');
 
   get gravatarUrl(): string {
-    const hash = this.emailOrHash().includes('@') ? sha256(this.emailOrHash()) : this.emailOrHash();
-    const size = this.size();
-    const defaultImage = this.defaultImage();
-    const rating = this.rating();
-
-    return `https://gravatar.com/avatar/${hash}?s=${size}&d=${defaultImage}&r=${rating}`;
+    return `https://gravatar.com/avatar/${this.emailHash()}?s=${this.size()}&d=${this.defaultImage()}&r=${this.rating()}`;
   }
 }
