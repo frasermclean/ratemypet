7759a14ea7b5499e7cd9973861e4ddd236491355
Fraser McLean <dev@frasermclean.com>
2024-12-04 11:17:48 +0800

    Add GetCurrentUser endpoint and UserResponse model

---
+++ backend/src/Api/Endpoints/Users/GetCurrentUserEndpoint.cs
@@ -0,0 +1,34 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using RateMyPet.Persistence.Models;
+
+namespace RateMyPet.Api.Endpoints.Users;
+
+public class GetCurrentUserEndpoint(UserManager<User> userManager)
+    : EndpointWithoutRequest<Results<Ok<UserResponse>, NotFound>>
+{
+    public override void Configure()
+    {
+        Get("/users/me");
+    }
+
+    public override async Task<Results<Ok<UserResponse>, NotFound>> ExecuteAsync(CancellationToken cancellationToken)
+    {
+        var user = await userManager.GetUserAsync(User);
+
+        if (user is null)
+        {
+            return TypedResults.NotFound();
+        }
+
+        var response = new UserResponse
+        {
+            Id = user.Id,
+            UserName = user.UserName!,
+            EmailAddress = user.Email!
+        };
+
+        return TypedResults.Ok(response);
+    }
+}
---
+++ backend/src/Api/Endpoints/Users/UserResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace RateMyPet.Api.Endpoints.Users;
+
+public class UserResponse
+{
+    public required Guid Id { get; init; }
+    public required string UserName { get; init; }
+    public required string EmailAddress { get; init; }
+}
