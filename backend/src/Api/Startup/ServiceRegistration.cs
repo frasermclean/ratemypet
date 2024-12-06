b6cff0590562000e530a1714091f3ef09889ff4b
Fraser McLean <dev@frasermclean.com>
2024-12-06 10:01:04 +0800

    Add token credential factory and refactor service registration code

--- backend/src/Api/Startup/ServiceRegistration.cs
+++ backend/src/Api/Startup/ServiceRegistration.cs
@@ -1,14 +1,16 @@
 ﻿using System.Text.Json;
 using System.Text.Json.Serialization;
-using Azure.Identity;
+using Azure.Storage.Blobs;
 using FastEndpoints;
 using Microsoft.AspNetCore.Http.Json;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Azure;
 using RateMyPet.Api.Options;
 using RateMyPet.Api.Services;
 using RateMyPet.Core;
+using RateMyPet.Persistence;
 using RateMyPet.Persistence.Services;
 
 namespace RateMyPet.Api.Startup;
@@ -18,11 +20,25 @@ public static class ServiceRegistration
     public static WebApplicationBuilder RegisterServices(this WebApplicationBuilder builder)
     {
         builder.Services
-            .AddApiServices()
             .AddPersistence(builder.Configuration)
             .AddIdentity()
-            .AddFastEndpoints();
+            .AddEnvironmentServices(builder.Environment, builder.Configuration)
+            .AddFastEndpoints()
+            .AddSingleton<ImageProcessor>();
 
+        builder.Services.AddOptions<ImageProcessorOptions>()
+            .BindConfiguration(ImageProcessorOptions.SectionName)
+            .ValidateDataAnnotations();
+
+        builder.Services.AddOptions<EmailSenderOptions>()
+            .BindConfiguration(EmailSenderOptions.SectionName)
+            .ValidateDataAnnotations();
+
+        builder.Services.AddOptions<FrontendOptions>()
+            .BindConfiguration(FrontendOptions.SectionName)
+            .ValidateDataAnnotations();
+
+        // problem details service
         builder.Services.AddProblemDetails(options =>
         {
             options.CustomizeProblemDetails = context =>
@@ -30,17 +46,6 @@ public static class ServiceRegistration
                 context.ProblemDetails.Instance = context.HttpContext.Request.Path;
                 context.ProblemDetails.Extensions.TryAdd("traceId", context.HttpContext.TraceIdentifier);
             };
-        });
-
-        builder.Services.AddAzureClients(factoryBuilder =>
-        {
-            var connectionString = builder.Configuration.GetConnectionString("Storage");
-            factoryBuilder.AddBlobServiceClient(connectionString);
-
-            var emailClientEndpoint = new Uri(builder.Configuration["EmailSender:Endpoint"]!);
-            factoryBuilder.AddEmailClient(emailClientEndpoint);
-
-            factoryBuilder.UseCredential(new DefaultAzureCredential());
         });
 
         // json serialization options
@@ -51,40 +56,35 @@ public static class ServiceRegistration
             options.SerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
         });
 
-        // development services
-        if (builder.Environment.IsDevelopment())
-        {
-            // add cors policy
-            builder.Services.AddCors(options =>
-            {
-                options.AddDefaultPolicy(policyBuilder => policyBuilder
-                    .WithOrigins(builder.Configuration["Frontend:BaseUrl"]!)
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowCredentials()
-                    .WithExposedHeaders("Location")
-                );
-            });
-        }
-
         return builder;
     }
 
-    private static IServiceCollection AddApiServices(this IServiceCollection services)
+    private static IServiceCollection AddPersistence(this IServiceCollection services,
+        ConfigurationManager configuration)
     {
-        services.AddSingleton<ImageProcessor>();
+        services.AddDbContextFactory<ApplicationDbContext>(builder =>
+        {
+            var connectionString = configuration.GetConnectionString("Database");
+            builder.UseSqlServer(connectionString);
+        });
 
-        services.AddOptions<ImageProcessorOptions>()
-            .BindConfiguration(ImageProcessorOptions.SectionName)
-            .ValidateDataAnnotations();
+        services.AddKeyedScoped<IBlobContainerManager>(BlobContainerNames.OriginalImages, (provider, _) =>
+        {
+            var containerClient = provider.GetRequiredService<BlobServiceClient>()
+                .GetBlobContainerClient(BlobContainerNames.OriginalImages);
+            return new BlobContainerManager(containerClient);
+        });
 
-        services.AddOptions<EmailSenderOptions>()
-            .BindConfiguration(EmailSenderOptions.SectionName)
-            .ValidateDataAnnotations();
+        services.AddAzureClients(factoryBuilder =>
+        {
+            var connectionString = configuration.GetConnectionString("Storage");
+            factoryBuilder.AddBlobServiceClient(connectionString);
 
-        services.AddOptions<FrontendOptions>()
-            .BindConfiguration(FrontendOptions.SectionName)
-            .ValidateDataAnnotations();
+            var emailClientEndpoint = new Uri(configuration["EmailSender:Endpoint"]!);
+            factoryBuilder.AddEmailClient(emailClientEndpoint);
+
+            factoryBuilder.UseCredential(TokenCredentialFactory.Create());
+        });
 
         return services;
     }
@@ -109,6 +109,29 @@ public static class ServiceRegistration
         services.AddTransient<IEmailSender, IdentityEmailSender>();
 
         services.AddHostedService<SecurityInitializer>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddEnvironmentServices(this IServiceCollection services,
+        IWebHostEnvironment environment, ConfigurationManager configuration)
+    {
+        if (!environment.IsDevelopment())
+        {
+            return services;
+        }
+
+        // cors policy for frontend development
+        services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(policyBuilder => policyBuilder
+                .WithOrigins(configuration["Frontend:BaseUrl"]!)
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials()
+                .WithExposedHeaders("Location")
+            );
+        });
 
         return services;
     }
