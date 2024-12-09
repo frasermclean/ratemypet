53a01018bc5c24e28f48bf3b35c30dd11ec0c529
Fraser McLean <dev@frasermclean.com>
2024-12-09 10:22:48 +0800

    Enhance NGXS plugin configuration to include development options for better debugging

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 import { withNgxsLoggerPlugin } from '@ngxs/logger-plugin';
+import { withNgxsDevelopmentOptions } from '@ngxs/store';
 import { Environment } from './environment.interface';
 
 export const environment: Environment = {
@@ -8,5 +9,10 @@ export const environment: Environment = {
     connectionString:
       'InstrumentationKey=113d2d85-aeb9-4e81-b6fa-62db294bbaa0;IngestionEndpoint=https://southeastasia-1.in.applicationinsights.azure.com/;LiveEndpoint=https://southeastasia.livediagnostics.monitor.azure.com/;ApplicationId=421044f1-5680-4fa5-8f34-72bbc3353ff3'
   },
-  ngxsPlugins: [withNgxsLoggerPlugin()]
+  ngxsPlugins: [
+    withNgxsDevelopmentOptions({
+      warnOnUnhandledActions: true
+    }),
+    withNgxsLoggerPlugin()
+  ]
 };
