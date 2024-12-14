a81b5db7bfe7de4386ee7414581df53b4985bd87
Fraser McLean <dev@frasermclean.com>
2024-12-14 11:34:47 +0800

    Add production environment values

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,9 +2,10 @@ import { Environment } from './environment.interface';
 
 export const environment: Environment = {
   name: 'production',
-  apiBaseUrl: '',
+  apiBaseUrl: 'https://api.ratemy.pet/api',
   applicationInsights: {
-    connectionString: ''
+    connectionString:
+      'InstrumentationKey=2c98db66-4f95-4583-bef8-efcc9fb22ac3;IngestionEndpoint=https://australiaeast-1.in.applicationinsights.azure.com/;LiveEndpoint=https://australiaeast.livediagnostics.monitor.azure.com/;ApplicationId=835e56be-f22b-4ab0-b88a-f234374fcdc5'
   },
   ngxsPlugins: []
 };
