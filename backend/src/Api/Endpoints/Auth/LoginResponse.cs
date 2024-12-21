b6cb8c041d708766fb73f7d613292fbabec2839b
Fraser McLean <dev@frasermclean.com>
2024-12-21 18:37:09 +0800

    Rename Id property to UserId in LoginResponse and related endpoints

--- backend/src/Api/Endpoints/Auth/LoginResponse.cs
+++ backend/src/Api/Endpoints/Auth/LoginResponse.cs
@@ -2,7 +2,7 @@
 
 public class LoginResponse
 {
-    public required Guid Id { get; init; }
+    public required Guid UserId { get; init; }
     public required string UserName { get; init; }
     public required string EmailAddress { get; init; }
     public required string EmailHash { get; init; }
