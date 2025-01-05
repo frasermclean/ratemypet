30b87f046db52bde3a81cb3dd92a6615ca89d35b
Fraser McLean <dev@frasermclean.com>
2025-01-05 19:53:54 +0800

    Update ClaimsPrincipalExtensions.GetUserId method and corresponding unit tests

--- backend/src/Api/Extensions/ClaimsPrincipalExtensions.cs
+++ backend/src/Api/Extensions/ClaimsPrincipalExtensions.cs
@@ -4,12 +4,15 @@ namespace RateMyPet.Api.Extensions;
 
 public static class ClaimsPrincipalExtensions
 {
+    /// <summary>
+    /// Attempts to get the user id from the claims principal.
+    /// </summary>
+    /// <param name="principal">The claims principal to process</param>
+    /// <returns>The user ID if found, null if not</returns>
     public static Guid? GetUserId(this ClaimsPrincipal principal)
     {
-        var claim = principal.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.NameIdentifier);
+        var value = principal.FindFirstValue(ClaimTypes.NameIdentifier);
 
-        return claim is not null
-            ? Guid.Parse(claim.Value)
-            : null;
+        return Guid.TryParse(value, out var userId) ? userId : null;
     }
 }
