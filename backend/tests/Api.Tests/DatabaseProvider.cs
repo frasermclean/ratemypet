8e83524df35c3f8a524c178c4480b2760fbf4950
Fraser McLean <dev@frasermclean.com>
2025-02-01 11:48:53 +0800

    Specify SQL server image

--- backend/tests/Api.Tests/DatabaseProvider.cs
+++ backend/tests/Api.Tests/DatabaseProvider.cs
@@ -5,6 +5,7 @@ namespace RateMyPet.Api;
 public sealed class DatabaseProvider : IAsyncDisposable
 {
     private readonly MsSqlContainer container = new MsSqlBuilder()
+        .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
         .Build();
 
     public string ConnectionString => container.GetConnectionString();
