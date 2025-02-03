1db9780b42fb79437885cd504b5b58aa0551ad07
Fraser McLean <dev@frasermclean.com>
2025-02-03 11:12:47 +0800

    Add function to process added posts

---
+++ backend/src/Jobs/Functions/ProcessAddedPost.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
+using RateMyPet.Core.Messages;
+using RateMyPet.Infrastructure;
+using RateMyPet.Infrastructure.Services;
+
+namespace RateMyPet.Jobs.Functions;
+
+public class ProcessAddedPost(ApplicationDbContext dbContext)
+{
+    [Function(nameof(ProcessAddedPost))]
+    public async Task ExecuteAsync([QueueTrigger(QueueNames.PostAdded)]
+        PostAddedMessage message,
+        CancellationToken cancellationToken)
+    {
+        // look up post by id
+        var post = await dbContext.Posts.FirstAsync(post => post.Id == message.PostId, cancellationToken);
+
+        // update post entity
+        post.IsAnalyzed = true;
+        await dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
