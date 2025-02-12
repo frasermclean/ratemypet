6029678fd4dfb58f03c27c637d066f5fc695d08a
Fraser McLean <dev@frasermclean.com>
2025-02-12 12:25:40 +0800

    Add GetPostStatusEndpoint to retrieve post status by ID or slug

---
+++ backend/src/Api/Endpoints/Posts/GetPostStatusEndpoint.cs
@@ -0,0 +1,37 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using RateMyPet.Core;
+using RateMyPet.Infrastructure.Services;
+
+namespace RateMyPet.Api.Endpoints.Posts;
+
+public class GetPostStatusEndpoint(ApplicationDbContext dbContext)
+    : EndpointWithoutRequest<Results<Ok<PostStatus>, NotFound>>
+{
+    public override void Configure()
+    {
+        Get("posts/{postId:guid}/status", "posts/{postSlug}/status");
+    }
+
+    public override async Task<Results<Ok<PostStatus>, NotFound>> ExecuteAsync(CancellationToken cancellationToken)
+    {
+        var postId = Route<Guid?>("postId", false);
+        var postSlug = Route<string?>("postSlug", false);
+
+        try
+        {
+            var status = await dbContext.Posts.AsNoTracking()
+                .Where(post => postId != null && post.Id == postId ||
+                               postSlug != null && post.Slug == postSlug)
+                .Select(post => post.Status)
+                .FirstAsync(cancellationToken);
+
+            return TypedResults.Ok(status);
+        }
+        catch (InvalidOperationException)
+        {
+            return TypedResults.NotFound();
+        }
+    }
+}
