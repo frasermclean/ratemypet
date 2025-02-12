08b07bde56bc8075aadf16b2072e152603bfa7b3
Fraser McLean <dev@frasermclean.com>
2025-02-12 18:54:43 +0800

    Add GetPopularTagsEndpoint to retrieve popular tags from posts

---
+++ backend/src/Api/Endpoints/Posts/GetPopularTagsEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using RateMyPet.Infrastructure.Services;
+
+namespace RateMyPet.Api.Endpoints.Posts;
+
+public class GetPopularTagsEndpoint(ApplicationDbContext dbContext) : EndpointWithoutRequest<IEnumerable<string>>
+{
+    public override void Configure()
+    {
+        Get("posts/popular-tags");
+        AllowAnonymous();
+    }
+
+    public override async Task<IEnumerable<string>> ExecuteAsync(CancellationToken cancellationToken)
+    {
+        var count = Query<int?>("count", false) ?? 10;
+
+        var tags = await dbContext.Posts
+            .SelectMany(post => post.Tags)
+            .GroupBy(tag => tag)
+            .OrderByDescending(group => group.Count())
+            .Take(count)
+            .Select(grouping => grouping.Key)
+            .ToListAsync(cancellationToken);
+
+        return tags;
+    }
+}
