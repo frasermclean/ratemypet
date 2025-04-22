bd5b9c16731d1d1660c7dbdc8e8fdd7ab472803b
Fraser McLean <dev@frasermclean.com>
2025-04-22 12:05:47 +0800

    Enhance post tags list: wrap tags in links for navigation and import RouterLink

--- frontend/src/app/posts/post-view/post-tags-list/post-tags-list.component.ts
+++ frontend/src/app/posts/post-view/post-tags-list/post-tags-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'post-tags-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './post-tags-list.component.html',
   styleUrl: './post-tags-list.component.scss'
 })
