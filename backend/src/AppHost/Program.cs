51c60ad0dc5520d8bc9b7f766dce0ae2357b3289
Fraser McLean <dev@frasermclean.com>
2025-05-13 10:34:29 +0800

    Add API project to App Host

--- backend/src/AppHost/Program.cs
+++ backend/src/AppHost/Program.cs
@@ -6,6 +6,8 @@ public static class Program
     {
         var builder = DistributedApplication.CreateBuilder(args);
 
+        var api = builder.AddProject<Projects.Api>("api");
+
         builder.Build().Run();
     }
 }
