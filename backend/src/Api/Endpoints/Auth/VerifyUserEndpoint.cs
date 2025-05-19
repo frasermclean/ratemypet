6d04ebe3622ff0807b411581dd8eaacc7d21298b
Fraser McLean <dev@frasermclean.com>
2025-05-19 13:06:32 +0800

    Refactor VerifyUserEndpoint to return VerifyUserResponse and add new VerifyUserResponse class

--- backend/src/Api/Endpoints/Auth/VerifyUserEndpoint.cs
+++ backend/src/Api/Endpoints/Auth/VerifyUserEndpoint.cs
@@ -8,26 +8,31 @@ using RateMyPet.Core;
 namespace RateMyPet.Api.Endpoints.Auth;
 
 public class VerifyUserEndpoint(UserManager<User> userManager)
-    : EndpointWithoutRequest<Ok<LoginResponse>>
+    : EndpointWithoutRequest<Ok<VerifyUserResponse>>
 {
     public override void Configure()
     {
         Get("/auth/verify-user");
+        AllowAnonymous();
     }
 
-    public override async Task<Ok<LoginResponse>> ExecuteAsync(CancellationToken cancellationToken)
+    public override async Task<Ok<VerifyUserResponse>> ExecuteAsync(CancellationToken cancellationToken)
     {
         var user = await userManager.GetUserAsync(User);
 
-        Debug.Assert(user is not null, "Authenticated user should never be null");
-
-        return TypedResults.Ok(new LoginResponse
+        return TypedResults.Ok(new VerifyUserResponse
         {
-            UserId = user.Id,
-            UserName = user.UserName!,
-            EmailAddress = user.Email!,
-            EmailHash = user.Email.ToSha256Hash(),
-            Roles = await userManager.GetRolesAsync(user)
+            IsAuthenticated = user is not null,
+            User = user is not null
+                ? new LoginResponse
+                {
+                    UserId = user.Id,
+                    UserName = user.UserName!,
+                    EmailAddress = user.Email!,
+                    EmailHash = user.Email.ToSha256Hash(),
+                    Roles = await userManager.GetRolesAsync(user)
+                }
+                : null
         });
     }
 }
