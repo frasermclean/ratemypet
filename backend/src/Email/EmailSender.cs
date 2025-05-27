3b168675b83f0ea93b911882caef776b1cb595f2
Fraser McLean <dev@frasermclean.com>
2025-05-27 09:45:44 +0800

    Refactor email sending logic

--- backend/src/Email/EmailSender.cs
+++ backend/src/Email/EmailSender.cs
@@ -12,8 +12,45 @@ public class EmailSender(
     EmailClient emailClient) : IEmailSender
 {
     private readonly string senderAddress = options.Value.SenderAddress;
+    private readonly string frontendBaseUrl = options.Value.FrontendBaseUrl;
 
-    public async Task SendEmailAsync(string recipientAddress, string subject, string htmlMessage,
+    public async Task SendConfirmationEmailAsync(string emailAddress, Guid userId, string confirmationToken,
+        CancellationToken cancellationToken = default)
+    {
+        var confirmationLink =
+            $"{frontendBaseUrl}/auth/confirm-email?userId={userId}&token={confirmationToken}";
+
+        var htmlMessage = $"""
+                           <html><body>
+                           <h1>Welcome to Rate My Pet!</h1>
+                           <p>Please confirm your account by <a href='{confirmationLink}'>clicking here</a>.</p>
+                           </body></html>
+                           """;
+
+        await SendEmailAsync(emailAddress, "Confirm your email", htmlMessage, cancellationToken);
+    }
+
+    public async Task SendPasswordResetEmailAsync(string emailAddress, string resetCode,
+        CancellationToken cancellationToken = default)
+    {
+        var resetLink =
+            $"{frontendBaseUrl}/auth/reset-password?emailAddress={emailAddress}&resetCode={resetCode}";
+
+        var htmlMessage = $"""
+                           <html><body>
+                           <h1>Password reset</h1>
+                           <p>
+                           A password reset request for your account has been received. If this was you, please
+                           <a href='{resetLink}'>click here</a> to reset your password.
+                           </p>
+                           <p>If you did not request a password reset, please you may safely ignore this email.</p>
+                           </body></html>
+                           """;
+
+        await SendEmailAsync(emailAddress, "Password reset", htmlMessage, cancellationToken);
+    }
+
+    private async Task SendEmailAsync(string recipientAddress, string subject, string htmlMessage,
         CancellationToken cancellationToken)
     {
         var content = new EmailContent(subject) { Html = htmlMessage };
