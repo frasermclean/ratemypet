4150fdaae2dd0bba91df57b69a5fed6cb3599c81
Fraser McLean <dev@frasermclean.com>
2025-06-02 15:04:51 +0800

    Refactor Angular SSR setup to Node.js express app

--- frontend/src/server.ts
+++ frontend/src/server.ts
@@ -1,14 +1,66 @@
-import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
+import {
+  AngularNodeAppEngine,
+  createNodeRequestHandler,
+  isMainModule,
+  writeResponseToNodeResponse
+} from '@angular/ssr/node';
+import type { Request, Response } from 'express';
+import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-const engine = new AngularAppEngine();
+const serverDistFolder = dirname(fileURLToPath(import.meta.url));
+const browserDistFolder = resolve(serverDistFolder, '../browser');
 
-/**
- * This is a request handler used by the Angular CLI (dev-server and during build).
- */
-export const reqHandler = createRequestHandler(async (request) => {
-  const response = await engine.handle(request);
+const app = express();
+const angularApp = new AngularNodeAppEngine();
 
-  return response ?? new Response('Page not found.', { status: 404 });
+const proxyMiddleware = createProxyMiddleware<Request, Response>({
+  target: process.env['API_BASE_URL'] || 'http://localhost:5080/api',
+  changeOrigin: true,
+  xfwd: true,
+  logger: console
 });
 
-export default { fetch: reqHandler };
+/**
+ * Forward API requests to backend server.
+ */
+app.use('/api', proxyMiddleware);
+
+/**
+ * Serve static files from /browser
+ */
+app.use(
+  express.static(browserDistFolder, {
+    maxAge: '1y',
+    index: false,
+    redirect: false
+  })
+);
+
+/**
+ * Handle all other requests by rendering the Angular application.
+ */
+app.use('/**', (req, res, next) => {
+  angularApp
+    .handle(req)
+    .then((response) => (response ? writeResponseToNodeResponse(response, res) : next()))
+    .catch(next);
+});
+
+/**
+ * Start the server if this module is the main entry point.
+ * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
+ */
+if (isMainModule(import.meta.url)) {
+  const port = process.env['PORT'] || 4000;
+  app.listen(port, () => {
+    console.log(`Node Express server listening on http://localhost:${port}`);
+  });
+}
+
+/**
+ * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
+ */
+export const reqHandler = createNodeRequestHandler(app);
