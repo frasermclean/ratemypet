eac699fa5b195259975fc9d43606cc1b4aad4ecd
Fraser McLean <dev@frasermclean.com>
2025-06-17 16:17:58 +0800

    Add endpoint and integration tests for AddPostReaction

---
+++ backend/src/Api/Endpoints/Posts/Reactions/AddPostReactionEndpoint.cs
@@ -0,0 +1,44 @@
+﻿using EntityFramework.Exceptions.Common;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http.HttpResults;
+using RateMyPet.Core;
+using RateMyPet.Database;
+
+namespace RateMyPet.Api.Endpoints.Posts.Reactions;
+
+public class AddPostReactionEndpoint(ApplicationDbContext dbContext) : Endpoint<AddPostReactionRequest, NoContent>
+{
+    public override void Configure()
+    {
+        Post("/posts/{postId:guid}/reactions");
+        Roles(Role.Contributor);
+    }
+
+    public override async Task<NoContent> ExecuteAsync(AddPostReactionRequest request,
+        CancellationToken cancellationToken)
+    {
+        var postReaction = MapToPostReaction(request);
+        dbContext.Add(postReaction);
+
+        try
+        {
+            await dbContext.SaveChangesAsync(cancellationToken);
+        }
+        catch (UniqueConstraintException exception) when (exception.ConstraintName == "PK_PostReactions")
+        {
+            Logger.LogError(exception, "Reaction already exists for user {UserId} on post {PostId}",
+                request.UserId, request.PostId);
+
+            ThrowError(r => r.Reaction, "You have already reacted to this post.");
+        }
+
+        return TypedResults.NoContent();
+    }
+
+    private static PostReaction MapToPostReaction(AddPostReactionRequest request) => new()
+    {
+        PostId = request.PostId,
+        UserId = request.UserId,
+        Reaction = request.Reaction
+    };
+}
